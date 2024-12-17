8fa33e9b82fc023cdd08ca593ebac3ec085f228d
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-12-17 16:54:11 +0100

    init

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Card({ post }) {
+  return;
+}
+
+export default Card;
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Card from "./Card";
+
+import styles from "../styles/Main.module.css";
+
+function Main() {
+  return (
+    <main>
+      <div className="container"></div>
+    </main>
+  );
+}
+
+export default Main;
