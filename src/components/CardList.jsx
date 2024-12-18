2deb8b4de6d19352dd379ee389b36e2ecfa9f9dc
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-12-18 01:34:51 +0100

    post form

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,26 +1,13 @@
-import React, { useState } from "react";
-
+import React from "react";
 import Card from "./Card";
 
-import initialPosts from "../data/posts";
-
-function CardList() {
-  const [posts, setPosts] = useState(initialPosts);
-
-  const handleDelete = (id) => {
-    setPosts((prevPosts) =>
-      prevPosts.map((post) =>
-        post.id === id ? { ...post, published: false } : post
-      )
-    );
-  };
-
+function CardList({ posts, onDelete }) {
   return (
     <div className="col-9 d-flex flex-wrap gap-3 justify-content-center">
       {posts
         .filter((post) => post.published)
         .map((post) => (
-          <Card post={post} key={post.id} onDelete={handleDelete} />
+          <Card post={post} key={post.id} onDelete={onDelete} />
         ))}
     </div>
   );
